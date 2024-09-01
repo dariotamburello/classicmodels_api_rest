ff69eeb381f4271759bec5e76ea062f17d09c5f5
Dario Tamburello <dariotamburello@hotmail.com>
2024-09-01 19:55:00 +0200

    add vercel domain to cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -3,7 +3,8 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
   'http://localhost:3000',
   'http://localhost:9900',
-  'http://192.168.100.52:3000'
+  'http://192.168.100.52:3000',
+  'https://classicmodels-api-rest.vercel.app/'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
