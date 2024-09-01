54b4f04e56cb5adcabe2aae0d481edf951af7006
Dario Tamburello <dariotamburello@hotmail.com>
2024-09-01 20:08:27 +0200

    update cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,7 +4,10 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:3000',
   'http://localhost:9900',
   'http://192.168.100.52:3000',
-  'https://classicmodels-api-rest.vercel.app/'
+  'https://classicmodels-api-rest.vercel.app/',
+  'https://classicmodels-api-rest.vercel.app/*',
+  '76.76.21.164:443',
+  'https://76.76.21.164:443'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
