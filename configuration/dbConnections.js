ef183a47fcccfac57778bb6ec0093f41708f4958
Dario Tamburello <dariotamburello@hotmail.com>
2024-09-02 20:00:38 +0200

    test

--- configuration/dbConnections.js
+++ configuration/dbConnections.js
@@ -3,7 +3,7 @@ if (process.env.ENV !== 'PRD') process.loadEnvFile()
 export const configuration = {
   host: process.env.DBHOST ?? 'localhost',
   port: process.env.DBPORT ?? '3306',
-  user: 'tdnic_prd3', // process.env.DBUSER ?? 'root',
+  user: 'tdnic_prd4', // process.env.DBUSER ?? 'root',
   password: process.env.DBPASSWORD ?? '',
   database: process.env.DBNAME ?? 'classicmodels'
 }
