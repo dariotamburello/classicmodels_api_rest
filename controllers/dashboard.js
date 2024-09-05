d8ff099a1193346e8a3856916e6237df4f2306ee
Dario Tamburello <dariotamburello@hotmail.com>
2024-09-05 13:31:14 +0200

    another test

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -560,9 +560,9 @@ export class DashboardController {
       console.time()
       const __dirname = path.dirname(__filename)
       const html = fs.readFileSync(path.join(__dirname, '../views/template-pdf.html'), 'utf-8')
+      console.timeEnd()
       const filename = Math.random().toString().substring(2) + '_doc' + '.pdf'
       let document
-      console.timeEnd()
 
       console.time()
       if (req.query.entity === 'products') {
@@ -592,8 +592,11 @@ export class DashboardController {
           path: './docs/' + filename
         }
       }
-      console.timeEnd()
 
+      // console.log(document)
+      return res.json(document.path)
+
+      console.timeEnd()
       console.time()
       const pdfFile = await pdf.create(document, options)
       if (pdfFile) {
